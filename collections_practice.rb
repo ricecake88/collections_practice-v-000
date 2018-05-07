f39f25aed376b6e787f2f59421fecbeec40007bf
Grace Shih <vocalise@gmail.com>
2018-05-07 21:40:46 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -4,20 +4,20 @@ end
 
 def sort_array_desc(integers)
   integers.sort do |a,b|
-    a <=> b
+    if a == b
+      0
+    elsif a > b
+      -1
+    else a < b
+      1
+    end
   end  
 end
 
 def sort_array_char_count(strings)
   ordered_strings = strings.sort
   ordered_strings.sort do |a,b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    else a.length > b.length
-      1
-    end
+    a.length <=> b.length
   end
 end
 
