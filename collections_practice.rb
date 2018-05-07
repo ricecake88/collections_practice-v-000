6e8ed9f61c318537631561563e1340f7796a8434
Grace Shih <vocalise@gmail.com>
2018-05-07 21:41:00 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -17,13 +17,7 @@ end
 def sort_array_char_count(strings)
   ordered_strings = strings.sort
   ordered_strings.sort do |a,b|
-    if a.length == b.length
-      0
-    elsif a.length < b.length
-      -1
-    else a.length > b.length
-      1
-    end
+    a.length <=> b.length
   end
 end
 
